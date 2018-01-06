cfcb015e8a3808691080825a835691f3555d60d7
Ahmad Arif <ahmad.arif019@gmail.com>
2018-01-06 10:59:05 +0700

    add router library

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = async (req, res) => {
-  res.end('Welcome to Micro-dev')
+const { json, send } = require('micro')
+const { router, get, post } = require('microrouter')
+
+const index = (req, res) => res.end('Welcome to Micro')
+const webhook = async (req, res) => {
+    console.log(req.headers)
+    const response = await json(req)
+    return response
 }
+
+module.exports = router(
+    get('/', index),
+    post('/', webhook)
+)
