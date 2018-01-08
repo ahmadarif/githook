b958eebb991a08f81362bd55a1a81b785bdfc53f
Ahmad Arif <ahmad.arif019@gmail.com>
2018-01-08 10:10:57 +0700

    update secret key validation function

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-const { buffer, json, send } = require('micro')
 const { router, get, post } = require('microrouter')
-const { sha1Github } = require('./utils')
-const shell = require('shelljs')
+const SecretKeyValidation = require('./lib/SecretKeyValidation')
+const { getIndex, postGithub } = require('./routes')
 
-const index = (req, res) => res.end('Welcome to Micro')
+const secretObj = SecretKeyValidation([
+    {
+        url: '/',
+        provider: 'github',
+        secret: 'rahasia'
+    }
+])
 
-const webhook = async (req, res) => {
-    const payload = await buffer(req)
-    const signDecrypt = sha1Github(payload, 'YOUR_OWN_SECRET')
-    const sig = req.headers['x-hub-signature']
-    const event = req.headers['x-github-event']
-
-    console.log('Is payload valid? ' + (signDecrypt == sig))
-    console.log('Event = ' + event)
-
-    shell.echo('Echo using shelljs')
-    /*
-    // pull if payload valid
-    exec('git pull origin master', function(code, stdout, stderr) {
-        console.log('Exit code:', code);
-        console.log('Program output:', stdout);
-        console.log('Program stderr:', stderr);
-    })
-    */
-
-    return { message: 'success' }
-}
-
-module.exports = router(
-    get('/', index),
-    post('/', webhook)
-)
+module.exports = secretObj(router(
+    get('/', getIndex),
+    post('/', postGithub)
+))
